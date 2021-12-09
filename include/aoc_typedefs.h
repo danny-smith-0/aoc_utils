b12414397e43be2eef45355dd7f83e3d99e9c0ce
Danny Smith <danny.smith.0@outlook.com>
2021-12-09 15:10:25 -0500

    Added typedefs

--- include/aoc_typedefs.h
+++ include/aoc_typedefs.h
@@ -5,6 +5,11 @@
 #include <vector>
 
 typedef std::vector<int> ints_t;
-typedef std::vector<std::string> strings_t;
+typedef std::vector<ints_t> ints_t_2d;
+typedef std::vector<bool> bools_t;
+typedef std::vector<bools_t> bools_t_2d;
+typedef std::vector<std::string> strings_t;\
+
+typedef std::pair<size_t, size_t> coords_t;
 
 #endif // AOC_TYPEDEFS_H
