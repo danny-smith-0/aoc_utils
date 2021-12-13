934e58e87a7e65e93c6747680ab4c164372e7ed9
Danny Smith <dsmith@areai.com>
2021-12-13 10:24:17 -0500

    Update template. sll's and result functions

--- dayN.cpp
+++ dayN.cpp
@@ -1,21 +1,40 @@
 #include <advent_of_code.h>
 
+// ints_t_2d parse()
+// {
+//     //
+// }
+
+sll part1(strings_t data)
+{
+    sll out = 0;
+    return out;
+}
+
+sll part2(strings_t data)
+{
+    sll out = 0;
+    return out;
+}
+
 int main ()
 {
-    std::string day_string = "10";
+    std::string day_string = "14";
     strings_t test_data = get_strings_from_file("../inputs/day" + day_string + "_test.txt");
     strings_t real_data = get_strings_from_file("../inputs/day" + day_string + ".txt");
     std::cout << "Day " << day_string << std::endl;
 
 
-    int results_test_1 = 0;
-    int results_real_1 = 0;
+    sll results_test_1 = part1(test_data);
+    sll results_real_1 = part1(real_data);
+    sll expected_test_result_1 = 0;
 
-    int results_test_2 = 0;
-    int results_real_2 = 0;
+    sll results_test_2 = part2(test_data);
+    sll results_real_2 = part2(test_data);
+    sll expected_test_result_2 = 0;
 
-    results(results_test_1, 0, results_real_1);
-    results(results_test_2, 0, results_real_2);
+    results(results_test_1, expected_test_result_1, results_real_1);
+    results(results_test_2, expected_test_result_2, results_real_2);
 
     return 0;
 }
