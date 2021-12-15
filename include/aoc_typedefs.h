c9cc5aa8598d0084afdc383416f6b1d9d3cfd5a0
Danny Smith <dsmith@areai.com>
2021-12-15 10:21:00 -0500

    Added slls_t and ulls_t & 2d versions

--- include/aoc_typedefs.h
+++ include/aoc_typedefs.h
@@ -7,12 +7,19 @@
 typedef unsigned long long ull;
 typedef signed long long sll;
 
-typedef std::vector<int> ints_t;
+typedef std::vector<int>    ints_t;
 typedef std::vector<ints_t> ints_t_2d;
-typedef std::vector<bool> bools_t;
+
+typedef std::vector<ull>    ulls_t;
+typedef std::vector<ulls_t> ulls_t_2d;
+typedef std::vector<sll>    slls_t;
+typedef std::vector<slls_t> slls_t_2d;
+
+typedef std::vector<bool>    bools_t;
 typedef std::vector<bools_t> bools_t_2d;
+
 typedef std::vector<std::string> strings_t;
-typedef std::vector<strings_t> strings_t_2d;
+typedef std::vector<strings_t>   strings_t_2d;
 
 typedef std::pair<size_t, size_t> coords_t;
 
