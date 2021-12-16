a9af6682eb2631f34eb8d4aeee5f441b2e50ef2a
Danny Smith <dsmith@areai.com>
2021-12-15 21:54:15 -0500

    const&'s for inputs in string parsers

--- source/string_parser.cpp
+++ source/string_parser.cpp
@@ -15,7 +15,7 @@ strings_t get_strings_from_file(const std::string& file_path)
 }
 
 
-bool find_space_in_string(std::string str_in, size_t* index)
+bool find_space_in_string(std::string const& str_in, size_t* index)
 {
     size_t found = str_in.find(" ");
     if(found != std::string::npos)
@@ -26,17 +26,17 @@ bool find_space_in_string(std::string str_in, size_t* index)
     return false;
 }
 
-std::string string_before_space(std::string string_in, size_t space_index)
+std::string string_before_space(std::string const& string_in, size_t space_index)
 {
     return string_in.substr(0, space_index);
 }
 
-std::string string_after_space(std::string string_in, size_t space_index)
+std::string string_after_space(std::string const& string_in, size_t space_index)
 {
     return string_in.substr(space_index + 1, string_in.size() - 1);
 }
 
-std::vector<int> substrings_to_ints(std::string string, std::string delimeter)
+std::vector<int> substrings_to_ints(std::string const& string, std::string const& delimeter)
 {
     std::vector<int> ints;
     std::stringstream s_stream(string);
@@ -51,7 +51,7 @@ std::vector<int> substrings_to_ints(std::string string, std::string delimeter)
     return ints;
 }
 
-strings_t substrings_to_strings(std::string string, std::string delimeter)
+strings_t substrings_to_strings(std::string const& string, std::string const& delimeter)
 {
     strings_t strings;
     std::stringstream s_stream(string);
@@ -66,7 +66,7 @@ strings_t substrings_to_strings(std::string string, std::string delimeter)
     return strings;
 }
 
-ints_t_2d strings_of_digits_to_ints(strings_t input)
+ints_t_2d strings_of_digits_to_ints(strings_t const& input)
 {
     ints_t_2d out;
     for (auto number_string : input)
