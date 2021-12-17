0aab1f974ca3c58114cfabd7460c88fd1aded95f
Danny Smith <danny.smith.0@outlook.com>
2021-12-17 10:07:03 -0500

    Adjust "results" funct & split out more aoc_utils to the cpp
    Except templates.

--- include/other_aoc_utils.h
+++ include/other_aoc_utils.h
@@ -5,34 +5,18 @@
 #include <iostream>
 #include <algorithm>
 
+// Replicate the matlab colon 2:5 ==> {2,3,4,5} and 6:4 ==> {6,5,4}
 ints_t matlab_colon(int first, int last);
 
-template <typename T>
-void results(T test_result, T expected_test_result, T real_result)
-{
-    std::cout << "Test result is " << test_result;
-    if (test_result == expected_test_result)
-    {
-        std::cout << ". Passed!\n";
-        std::cout << "Real result is " << real_result << std::endl;
-    }
-    else
-        std::cout << ". Failed. Looking for " << expected_test_result << "\n";
-}
+// Count the number of true's in a bools_t_2d
+int count_bools_t_2d(bools_t_2d bools);
 
-void print_ints_t_2d(ints_t_2d output, bool with_comma = false)
-{
-    for (auto row : output)
-    {
-        for (auto val : row)
-        {
-            std::cout << val;
-            if (with_comma)
-                std::cout << ",";
-        }
-        std::cout << std::endl;
-    }
-}
+// Given a path (vector or coordinates), make a bools_t_2d with true set for the path locations
+bools_t_2d coords_to_bools(coords_t_vec path, size_t max_dimension = 1001, size_t min_dimension_display = 0);
+
+// Get the number of non-diagonal steps between two coordinates
+size_t steps_between_cells(coords_t cell1, coords_t cell2);
+
 
 template <typename T>
 void print_T_2d(T output, bool with_comma = false, bool null_is_space = false)
@@ -57,49 +41,6 @@ std::string print_T_2d_to_string(T output, bool with_comma = false, bool null_is
     return ss.str();
 }
 
-int count_bools_t_2d(bools_t_2d bools)
-{
-    int count = 0;
-    for (auto row: bools)
-        for (auto my_bool : row)
-            count += my_bool ? 1 : 0;
-
-    return count;
-}
-
-bools_t_2d coords_to_bools(coords_t_vec path, size_t max_dimension = 1001, size_t min_dimension_display = 0)
-{
-    // min_dimension_display = min_dimension_display == 0 ? max_dimension :
-    bools_t_2d map (max_dimension, bools_t(max_dimension, false));
-    size_t my_max_idx = 0;
-    for (auto coords : path)
-    {
-        my_max_idx = std::max(my_max_idx, std::max(coords.first, coords.second));
-        if(my_max_idx + 1 > max_dimension)
-        {
-            std::cout << "my_max_idx + 1 > max_dimension! ---  " << my_max_idx << " + 1 > " << max_dimension << ". Uh-oh\n";
-            return bools_t_2d();
-        }
-        map[coords.first][coords.second] = true;
-    }
-
-    //Remove unused parts of the map
-    min_dimension_display = std::max(min_dimension_display, my_max_idx + 1);
-    map = bools_t_2d(map.begin(), map.begin() + min_dimension_display);
-    for (auto& line : map)
-        line = bools_t(line.begin(), line.begin() + min_dimension_display);
-    return map;
-}
-
-size_t steps_between_cells(coords_t cell1, coords_t cell2)
-{
-    sll diff = cell1.first - cell2.first;
-    size_t first_steps  = static_cast<size_t>(std::abs(diff));
-    diff = cell1.second - cell2.second;
-    size_t second_steps = static_cast<size_t>(std::abs(diff));
-    size_t out = first_steps + second_steps;
-    return out;
-}
 
 template <typename T>
 size_t largest_subvector(std::vector<std::vector<T>> vec_2d)
@@ -117,6 +58,23 @@ size_t smallest_subvector(std::vector<std::vector<T>> vec_2d)
     for (auto vec : vec2d)
         max_size = vec.size() < max_size ? vec.size() : max_size;
     return max_size;
+}
+
+
+template <typename T>
+void results(T test_result, T expected_test_result, T real_result)
+{
+    std::cout << "Test result is " << test_result;
+    if (test_result == expected_test_result) { std::cout << ". Passed!\nReal result is " << real_result << std::endl; }
+    else                                     { std::cout << ". Failed. Looking for " << expected_test_result << "\n";  }
+}
+
+template <typename T>
+bool results(T result, T expected_result)
+{
+    std::cout << "Test result is " << result;
+    if (result == expected_result) { std::cout << ". Passed!\n Now running the real input\n";          return true;  }
+    else                           { std::cout << ". Failed. Looking for " << expected_result << "\n"; return false; }
 }
 
 #endif // OTHER_AOC_UTILS_H
--- source/other_aoc_utils.cpp
+++ source/other_aoc_utils.cpp
@@ -1,5 +1,6 @@
 #include <other_aoc_utils.h>
 
+// Replicate the matlab colon 2:5 ==> {2,3,4,5} and 6:4 ==> {6,5,4}
 ints_t matlab_colon(int first, int last)
 {
     int incr = first < last ? 1 : -1;
@@ -11,4 +12,51 @@ ints_t matlab_colon(int first, int last)
             break;
     }
     return ints;
+}
+
+// Count the number of true's in a bools_t_2d
+int count_bools_t_2d(bools_t_2d bools)
+{
+    int count = 0;
+    for (auto row: bools)
+        for (auto my_bool : row)
+            count += my_bool ? 1 : 0;
+
+    return count;
+}
+
+// Given a path (vector or coordinates), make a bools_t_2d with true set for the path locations
+bools_t_2d coords_to_bools(coords_t_vec path, size_t max_dimension /*= 1001*/, size_t min_dimension_display /*= 0*/)
+{
+    // min_dimension_display = min_dimension_display == 0 ? max_dimension :
+    bools_t_2d map (max_dimension, bools_t(max_dimension, false));
+    size_t my_max_idx = 0;
+    for (auto coords : path)
+    {
+        my_max_idx = std::max(my_max_idx, std::max(coords.first, coords.second));
+        if(my_max_idx + 1 > max_dimension)
+        {
+            std::cout << "my_max_idx + 1 > max_dimension! ---  " << my_max_idx << " + 1 > " << max_dimension << ". Uh-oh\n";
+            return bools_t_2d();
+        }
+        map[coords.first][coords.second] = true;
+    }
+
+    //Remove unused parts of the map
+    min_dimension_display = std::max(min_dimension_display, my_max_idx + 1);
+    map = bools_t_2d(map.begin(), map.begin() + min_dimension_display);
+    for (auto& line : map)
+        line = bools_t(line.begin(), line.begin() + min_dimension_display);
+    return map;
+}
+
+// Get the number of non-diagonal steps between two coordinates
+size_t steps_between_cells(coords_t cell1, coords_t cell2)
+{
+    sll diff = cell1.first - cell2.first;
+    size_t first_steps  = static_cast<size_t>(std::abs(diff));
+    diff = cell1.second - cell2.second;
+    size_t second_steps = static_cast<size_t>(std::abs(diff));
+    size_t out = first_steps + second_steps;
+    return out;
 }
