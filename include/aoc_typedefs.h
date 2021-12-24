ffa21334062f22a941eef47f488fceade8bd08f9
Danny Smith <danny.smith.0@outlook.com>
2021-12-23 23:57:47 -0500

    ints3d and bools3d

--- include/aoc_typedefs.h
+++ include/aoc_typedefs.h
@@ -9,6 +9,7 @@ typedef signed long long sll;
 
 typedef std::vector<int>    ints_t;
 typedef std::vector<ints_t> ints_t_2d;
+typedef std::vector<ints_t_2d> ints_t_3d;
 
 typedef std::vector<ull>    ulls_t;
 typedef std::vector<ulls_t> ulls_t_2d;
@@ -17,6 +18,7 @@ typedef std::vector<slls_t> slls_t_2d;
 
 typedef std::vector<bool>    bools_t;
 typedef std::vector<bools_t> bools_t_2d;
+typedef std::vector<bools_t_2d> bools_t_3d;
 
 typedef std::vector<std::string> strings_t;
 typedef std::vector<strings_t>   strings_t_2d;
